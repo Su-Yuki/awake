6d41c3905f3553836fc4d37fa46dd1001ed78f40
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-06-25 00:51:49 +0900

    Feat: add react module

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,12 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('app');
+// });
 
-Route::get('test', 'HemeController@index');
+// Route::get('test', 'HemeController@index');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('{any}', function () {
+    return view('layouts.app');
+})->where('any','.*');
