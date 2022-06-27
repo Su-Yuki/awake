51a2590147c4d13e5b0ab4c0ec54cbca4ef4c22f
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-06-27 15:58:42 +0900

    feat: added for thema rooting

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,10 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
+Route::post('/register', 'App\Http\Controllers\Api\LoginController@register');
+Route::post('/login', 'App\Http\Controllers\Api\LoginController@login');
+Route::get('/logout', 'App\Http\Controllers\Api\LoginController@logout');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::get('thema', 'App\Http\Controllers\ThemaController@index');
+//     Route::post('thema/store', 'App\Http\Controllers\ThemaController@store');
 // });
 
+
 Route::group(['middleware' => 'api'], function(){
-    Route::get('test', 'App\Http\Controllers\TopController@index');
+    Route::get('thema', 'App\Http\Controllers\ThemaController@index');
+    Route::post('thema/store', 'App\Http\Controllers\ThemaController@store');
 });
