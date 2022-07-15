cf41b9cdc7de3d2d2e3e3d5dffe858b3cf891900
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-07-16 05:42:04 +0900

    feat: Added confirmation dialog for having children at the front

--- laravel/resources/ts/components/ThemaListMenu.tsx
+++ laravel/resources/ts/components/ThemaListMenu.tsx
@@ -11,7 +11,15 @@ import { Thema } from '../type/Thema';
 
 /* material-ui */
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { Menu, MenuItem, Button } from '@mui/material';
+import {
+  Menu,
+  MenuItem,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@mui/material';
 
 /* material-ui icon */
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
@@ -44,30 +52,31 @@ const useStyles = makeStyles((theme) =>
 // ---[ process ]---------------------------------------------------------------
 export const ThemaListMenu = (props: any) => {
   // state
-  const {themas, setThemas}     = useContext(ThemaContexts);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const {themas, setThemas}         = useContext(ThemaContexts);
+  const [anchorEl, setAnchorEl]     = useState<null | HTMLElement>(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   // set value
   const data  = props.data;
   const index = props.index;
 
   // set other value
-  const open     = Boolean(anchorEl);
+  const menuOpen = Boolean(anchorEl);
   const classes  = useStyles();
 
   // menu open & close method
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(e.currentTarget);
   };
-  const handleClose = () => {
+  const handleCloseMenu = () => {
     setAnchorEl(null);
   };
 
   // Delete thema method
-  const onDeleteThema = async(id: number, index: number) => {
+  const onDeleteThema = async(data: Thema, index: number) => {
     try {
       // DB
-      await axios.delete(`//localhost/api/thema/delete/${id}`)
+      await axios.delete(`//localhost/api/thema/delete/${data.id}`)
       // delete theme context {id} object
       const newThemas = themas.filter((thema, childindex) => childindex !== index);
       setThemas(newThemas);
@@ -77,32 +86,66 @@ export const ThemaListMenu = (props: any) => {
     setAnchorEl(null);
   }
 
+  const handleClickDialog = (data: Thema, index: number) => {
+    if(data.inner_word_count > 0){
+      setOpenDialog(true);
+    } else {
+      setAnchorEl(null);
+      onDeleteThema(data, index)
+    }
+  };
+
+  const handleCloseDialog = () => {
+    setAnchorEl(null);
+    setOpenDialog(false);
+  };
+
   return (
     <>
       <Button
         className={classes.more_horiz_button}
         id='basic-button'
-        aria-controls={open ? 'basic-menu' : undefined}
+        aria-controls={menuOpen ? 'basic-menu' : undefined}
         aria-haspopup='true'
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
+        aria-expanded={menuOpen ? 'true' : undefined}
+        onClick={handleClickMenu}
       >
         <MoreHorizIcon className={classes.more_horiz_icon} />
       </Button>
       <Menu
         id='basic-menu'
         anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
+        open={menuOpen}
+        onClose={handleCloseMenu}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
       >
         <MenuItem
           className={classes.more_horiz_menu_item}
-          onClick={() => (onDeleteThema(data.id, index))}
+          onClick={() => (handleClickDialog(data, index))}
         >削除
         </MenuItem>
+        <Dialog
+          open={openDialog}
+          onClose={handleCloseDialog}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {`テーマ「${data.thema}」は、${data.inner_word_count}個の内なる言葉があります。削除しますか？`}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog}>キャンセル</Button>
+            <Button
+              onClick={() => (onDeleteThema(data, index))}
+              autoFocus
+            >削除する
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Menu>
     </>
   )
